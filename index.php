78970f8f9385971e87b74b4678d0fc72b9bb7ead
Vincent <vincent.composieux@gmail.com>
2011-08-07 02:38:08 +0200

    Add configuration files

--- index.php
+++ index.php
@@ -7,11 +7,22 @@
  * @author Vincent Composieux <vincent.composieux@gmail.com>
  */
 
+define('APPLICATION_ENV', 'local');
+
 require_once 'src/Forest/Bootstrap.php';
 
-$forest = new Forest\Bootstrap(
-    array('debug' => true)
-);
+// Load configuration
+$options = array();
+
+$configFile = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'settings.ini';
+$config = parse_ini_file($configFile, true);
+
+if (true === defined('APPLICATION_ENV') && true === isset($config[APPLICATION_ENV])) {
+    $options = $config[APPLICATION_ENV];
+}
+
+// Bootstrap
+$forest = new Forest\Bootstrap($options);
 
 echo $forest->getDuration();
 ?>
