75023a6d437e234603088be8ea7f64ae0fc9ff98
Vincent <vincent.composieux@gmail.com>
2011-08-23 23:19:39 +0200

    Implementing Symfony2 Yaml Component to load configuration

--- index.php
+++ index.php
@@ -11,20 +11,12 @@ require_once 'src/Forest/Bootstrap.php';
 
 define('APPLICATION_ENV', 'local');
 
-// Load configuration
-$configFile = dirname(__FILE__) . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'settings.ini';
-$config = parse_ini_file($configFile, true);
-
-$options = array();
-
-if (true === defined('APPLICATION_ENV') && true === isset($config[APPLICATION_ENV])) {
-    $options = array_merge($config['all'], $config[APPLICATION_ENV]);
-} else {
-    throw new \Exception('You need to set an APPLICATION_ENV section in config/settings.ini and specify foREST environment');
-}
-
-// Bootstrap
-$forest = new Forest\Bootstrap($options);
+// Bootstrap application and components
+$forest = new Forest\Bootstrap(
+    array(
+        'Yaml' => 'Symfony\Component\Yaml\Yaml'
+    )
+);
 
 echo $forest->getDuration();
 ?>
--- src/Forest/Bootstrap.php
+++ src/Forest/Bootstrap.php
@@ -10,6 +10,7 @@
 namespace Forest;
 
 use Forest\Core\Dispatcher as Dispatcher;
+use Forest\Core\Exception as Exception;
 use Forest\Core\Registry as Registry;
 
 use Forest\Logger as Logger;
@@ -19,6 +20,12 @@ use Forest\Logger as Logger;
  */
 class Bootstrap
 {
+    /**
+     * Components loaded
+     * @var array
+     */
+    private $_components = array();
+    
     /**
      * Options availables
      * @var array
@@ -34,15 +41,16 @@ class Bootstrap
     /**
      * Constructor
      * 
-     * @param array $options
+     * @param array $components
      */
-    public function __construct($options = array()) {
-        $this->_options = $options;
-
+    public function __construct($components = array()) {
         $start = microtime(true);
+        
+        $this->_components = $components;
         
         spl_autoload_register(__CLASS__ .'::autoload');
         
+        $this->loadConfiguration();
         $this->loadResources();
         
         $dispatcher = new Dispatcher();
@@ -66,6 +74,25 @@ class Bootstrap
         if (true === file_exists($classfile)) {
             include $classfile;
         }
+    }
+    
+    /**
+     * Load configuration
+     */
+    private function loadConfiguration() {
+        if (false === array_key_exists('Yaml', $this->_components)) {
+            throw new Exception("Yaml component isn't loaded. You need to specify it on Bootstrap constructor");
+        }
+        
+        $basedir = realpath(dirname(__FILE__) . str_repeat(DIRECTORY_SEPARATOR . '..', 2));
+        $config = $basedir . DIRECTORY_SEPARATOR . 'config/configuration.yml';
+        
+        if (false === file_exists($config)) {
+            throw new Exception(sprintf('Configuration file does not exists at location: %s', $config));
+        }
+        
+        $yaml = new $this->_components['Yaml']();
+        $this->_options = $yaml->parse(file_get_contents($config));
     }
     
     /**
