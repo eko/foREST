22dcf158e08fab7a5786ecfce8520034fb2cfe17
Vincent Composieux <vincent.composieux@gmail.com>
2011-09-15 13:41:52 +0200

    Adding forgotten space

--- src/Forest/Bootstrap.php
+++ src/Forest/Bootstrap.php
@@ -27,7 +27,7 @@ class Bootstrap
      * @param string $environment
      */
     public function __construct($environment = null) {
-        spl_autoload_register(__CLASS__ .'::autoload');
+        spl_autoload_register(__CLASS__ . '::autoload');
         
         $this->kernel = new Kernel($environment);
         
